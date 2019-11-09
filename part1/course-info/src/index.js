97d76220d1362d68a4b97ec167e14336e2cfc9e7
marimontero <maria.montero@devex.com>
2019-11-09 10:59:31 +0100

    Part 1 Exc.1.4 Course information

--- part1/course-info/src/index.js
+++ part1/course-info/src/index.js
@@ -16,6 +16,7 @@ const Part = (props) => {
 }
 
 const Content = (props) => {
+  console.log(props);
   return(
     <div>
       <Part content={props.content[0]}/>
@@ -33,27 +34,26 @@ const Total = (props) => {
 
 const App = (props) => {
   const course = 'Half Stack application development'
-
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10
-  }
-
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7
-  }
-
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14
-  }
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
 
   return (
     <div>
       <Header course={course}/>
-      <Content content={[part1, part2, part3]}/>
-      <Total total={[part1, part2, part3]}/>
+      <Content content={parts}/>
+      <Total total={parts}/>
     </div>
   )
 }

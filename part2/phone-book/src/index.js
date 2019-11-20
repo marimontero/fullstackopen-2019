39fdd9a272a59ae7dfc560ece5643b2adca67e14
marimontero <maria.montero@devex.com>
2019-11-20 15:48:36 +0100

    Part 2 Exc.2.8 The Phonebook Step 3

--- part2/phone-book/src/index.js
+++ part2/phone-book/src/index.js
@@ -1,25 +1,30 @@
 import React, { useState} from 'react'
 import ReactDOM from 'react-dom'
 
-const ShowNames = (persons) => {
+const ShowPerson = (persons) => {
   return persons.map(
-    person => <li key={person.name}>{person.name}</li>
+    person => <li key={person.name}>{person.name} {person.number}</li>
   )
 }
 
 const App = () => {
 
   const [ persons, setPersons] = useState([
-    { name: 'Arto Hellas'}
+    { name: 'Arto Hellas', number:'34 635 981 197'}
   ])
 
   const [ newName, setNewName ] = useState('')
+  const [ newNumber, setNewNumber] = useState('')
 
   const handleNameChange = (event) => {
     setNewName(event.target.value)
   }
 
-  const handleAddName = (event) => {
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value)
+  }
+
+  const handleAddPerson = (event) => {
     event.preventDefault();
 
     const handleCheckName = persons.some(
@@ -29,15 +34,16 @@ const App = () => {
     if (handleCheckName) {
       alert(`${newName} is already added to the phonebook`);
     } else {
-      setPersons(persons.concat({name: newName}));
+      setPersons(persons.concat({name: newName, number: newNumber}));
       setNewName('')
+      setNewNumber('')
     }
   }
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <form onSubmit={handleAddName}>
+      <form onSubmit={handleAddPerson}>
         <div>
           <strong>Name: </strong>
           <input
@@ -46,11 +52,18 @@ const App = () => {
           />
         </div>
         <div>
+          <strong>Number: </strong>
+          <input
+            value={newNumber}
+            onChange={handleNumberChange}
+          />
+        </div>
+        <div>
           <button type="submit">Add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      <ul style={{listStyleType: "none", padding: "0"}}>{ShowNames(persons)}</ul>
+      <ul style={{listStyleType: "none", padding: "0"}}>{ShowPerson(persons)}</ul>
     </div>
   )
 }

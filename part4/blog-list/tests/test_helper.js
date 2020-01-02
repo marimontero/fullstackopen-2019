b105f058fbe02ddd6f24c5e3a861b576cb00e8e7
marimontero <maria.montero@devex.com>
2020-01-02 17:31:43 +0100

    Part 4 Exc 4.8 helper Blog list tests, step 1

---
+++ part4/blog-list/tests/test_helper.js
@@ -0,0 +1,18 @@
+const initialBlogs = [
+  {
+    title: 'HTML is easy',
+    author: 'Miquel Marti i Pol',
+    url: 'http://htmliseasy.com',
+    likes: 3
+  },
+  {
+    title: 'Browser can execute only Javascript',
+    author: 'Lluis Companys',
+    url: 'http://browserCanExecuteOnlyJavascript.com',
+    likes: 2
+  }
+]
+
+module.exports = {
+  initialBlogs
+}

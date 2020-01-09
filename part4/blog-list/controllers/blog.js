3f2310ef9f28bf9a3fcb38aab85ae73dc62d2ab7
marimontero <maria.montero@devex.com>
2020-01-09 16:45:11 +0100

    Part 4 Exc 4.18-19 Blog list expansions, step 6/7

--- part4/blog-list/controllers/blog.js
+++ part4/blog-list/controllers/blog.js
@@ -1,6 +1,16 @@
 const blogRouter = require('express').Router()
 const Blog = require('../models/blog')
 const User = require('../models/users')
+const config = require('../utils/config')
+const jwt = require('jsonwebtoken')
+
+const getTokenFrom = request => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+    return authorization.substring(7)
+  }
+  return null
+}
 
 blogRouter.get('/', async (request, response) => {
   const blogs = await Blog.find({}).populate('user', {
@@ -13,17 +23,27 @@ blogRouter.get('/', async (request, response) => {
 
 blogRouter.post('/', async (request, response, next) => {
   const body = request.body
-  const user = await User.findOne({})
-
-  const blog = new Blog({
-    title: body.title,
-    author: body.author,
-    url: body.url,
-    likes: body.likes,
-    user: user._id
-  })
+  const token = getTokenFrom(request)
 
   try {
+    const decodedToken = jwt.verify(token, config.SECRET)
+
+    if (!token || !decodedToken.id) {
+      return response.status(400).json({
+        error: 'token missing or invalid'
+      })
+    }
+
+    const user = await User.findById(decodedToken.id)
+
+    const blog = new Blog({
+      title: body.title,
+      author: body.author,
+      url: body.url,
+      likes: body.likes,
+      user: user._id
+    })
+
     const savedBlog = await blog.save()
     user.blogs = user.blogs.concat(savedBlog._id)
     await user.save()
--- part4/blog-list/utils/config.js
+++ part4/blog-list/utils/config.js
@@ -2,8 +2,10 @@ require('dotenv').config()
 
 const PORT = process.env.PORT
 const MONGODB_URI = `mongodb+srv://marimontero:${process.env.MONGODB_URI}@cluster0-tnsll.mongodb.net/test?retryWrites=true&w=majority`
+const SECRET = process.env.MONGODB_URI
 
 module.exports = {
   PORT,
-  MONGODB_URI
+  MONGODB_URI,
+  SECRET
 }

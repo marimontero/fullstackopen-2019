58eb95c260bfa479c8a916535d586bda91c014d3
marimontero <maria.montero@devex.com>
2020-01-10 20:13:06 +0100

    Part 5 bloglist frontend, step 5

--- part5/bloglist-frontend/src/components/UserBlogs.js
+++ part5/bloglist-frontend/src/components/UserBlogs.js
@@ -1,6 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Blog from './Blog'
 import CreateBlogs from './CreateBlogs'
+
+const LoginFormContainer = (props) => {
+  const [loginVisible, setLoginVisible] = useState(false)
+  const hideWhenVisible = { display: loginVisible ? 'none' : '' }
+  const showWhenVisible = { display: loginVisible ? '' : 'none' }
+
+  return (
+    <div>
+      <div style={hideWhenVisible}>
+        <button onClick={() => setLoginVisible(true)}>Create Blog</button>
+      </div>
+      <div style={showWhenVisible}>
+        {props.children}
+        <button onClick={() => setLoginVisible(false)}>cancel</button>
+      </div>
+    </div>
+  )
+}
 
 const UserBlogs = (props) => (
   <div>
@@ -10,14 +28,16 @@ const UserBlogs = (props) => (
       Logout
     </button>
     <div>
-    <CreateBlogs
-      blogs={props.blogs}
-      setBlogs={props.setBlogs}
-      newBlog={props.newBlog}
-      handleBlogChange={props.handleBlogChange}
-      setNotification={props.setNotification}
-      setNewError={props.setNewError}
-    />
+    <LoginFormContainer>
+      <CreateBlogs
+        blogs={props.blogs}
+        setBlogs={props.setBlogs}
+        newBlog={props.newBlog}
+        handleBlogChange={props.handleBlogChange}
+        setNotification={props.setNotification}
+        setNewError={props.setNewError}
+      />
+    </LoginFormContainer>
     </div>
     <div style={{marginTop: "10px"}}>
       {props.blogs.map(blog =>

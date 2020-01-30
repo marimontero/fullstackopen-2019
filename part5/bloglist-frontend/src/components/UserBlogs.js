ddbb56208d85c00b74f6c6f14c5a1e7b51e26786
marimontero <maria.montero@devex.com>
2020-01-30 18:05:06 +0100

    Part 5 bloglist frontend, step 8

--- part5/bloglist-frontend/src/components/UserBlogs.js
+++ part5/bloglist-frontend/src/components/UserBlogs.js
@@ -40,9 +40,12 @@ const UserBlogs = (props) => (
     </LoginFormContainer>
     </div>
     <div style={{marginTop: "10px"}}>
-      {props.blogs.map(blog =>
-        <Blog key={blog.id} blog={blog} handleLike={props.handleLike}/>
-      )}
+      {props.blogs
+        .sort((a, b) => b.likes - a.likes)
+        .map(blog =>
+          <Blog key={blog.id} blog={blog} handleLike={props.handleLike}/>
+        )
+      }
     </div>
   </div>
 )
